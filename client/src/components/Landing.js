e9c9ba504db27800aa0c54603a96e70c24355ea8
Miguel Silva <msilva0897@gmail.com>
2018-02-26 01:22:30 -0700

    youtube video

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import YouTube from 'react-youtube';
 
 const Landing = () => {
   return (
-    <div style={{ textAlign: 'center'}}>
-      <h1>
-        Joga Bonito
-      </h1>
+    <div>
+      <div style={{ textAlign: 'center'}}>
+        <h1>
+          Joga Bonito
+        </h1>
+        <h5> The place where every Futbol fan finds his next challenge!</h5>
+        <br></br>
+          <YouTube
+            className="z-depth-5"
+            videoId="rz-xJeiOxC4"
+            />
+      </div>
     </div>
   )
 }
